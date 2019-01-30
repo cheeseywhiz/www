c3fbbb18dc5e166cd22f54508fa468c3c2978caf
Spencer Todd <cheesy.mac.dott@gmail.com>
2019-01-30 14:05:52 -0500

    Move front end to front folder

--- front/deploy.sh
+++ front/deploy.sh
@@ -4,5 +4,5 @@ HOST=$(git config deploy.host)
 PORT=$(git config deploy.port)
 rm -rf build
 yarn build
-ssh -p $PORT $HOST rm -rfv build
-scp -P $PORT -r build $HOST:
+ssh -p $PORT $HOST rm -rfv www/front/build
+scp -P $PORT -r build $HOST:www/front
