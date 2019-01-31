72e2444d9843d8c45939807ca8c6a35ef4057217
Spencer Todd <cheesy.mac.dott@gmail.com>
2019-01-31 18:19:10 -0500

    Add id(1) endpoint

--- api/app.py
+++ api/app.py
@@ -1,12 +1,18 @@
 import flask
 import time
+import subprocess
 
 app = flask.Flask(__name__)
 
 
-@app.route('/api/')
+@app.route('/api/time')
 def index():
     return flask.make_response(
         str(int(time.time())),
         {'Cache-Control': 'no-cache'},
     )
+
+
+@app.route('/api/id')
+def id():
+    return subprocess.run('id', capture_output=True).stdout.decode()
