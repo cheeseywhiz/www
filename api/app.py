b8945b1183b0c4f15210d787fe29517cb089b128
Spencer Todd <cheesy.mac.dott@gmail.com>
2019-02-01 22:00:51 -0500

    Add sysinfo api endpoint

--- api/app.py
+++ api/app.py
@@ -3,8 +3,24 @@ import time
 import os
 import pwd
 import grp
+import ctypes
+import struct
+import functools
+import dataclasses
+import typing
 
+
+class DataclassJSONEncoder(flask.json.JSONEncoder):
+    def default(self, obj):
+        if dataclasses.is_dataclass(obj):
+            return dataclasses.asdict(obj)
+
+        return super().default(obj)
+
+
+libc = ctypes.CDLL('libc.so.6')
 app = flask.Flask(__name__)
+app.json_encoder = DataclassJSONEncoder
 
 
 @app.route('/api/time')
@@ -31,3 +47,65 @@ def id():
             for group in groups_data
         ],
     )
+
+
+def divide_seconds(seconds, blocks=[1, 60, 60, 24, 365]):
+    divisions = []
+    remainder = seconds
+    block_size_seconds = functools.reduce(lambda x, y: x * y, blocks)
+
+    for block_size in reversed(blocks):
+        division = remainder // block_size_seconds
+        remainder -= division * block_size_seconds
+        divisions.append(division)
+        block_size_seconds //= block_size
+
+    divisions.reverse()
+    return divisions
+
+
+@dataclasses.dataclass
+class Uptime:
+    total_seconds: int
+    seconds: int
+    minutes: int
+    hours: int
+    days: int
+    years: int
+
+    @classmethod
+    def from_total_seconds(cls, total_seconds):
+        return cls(total_seconds, *divide_seconds(total_seconds))
+
+
+@dataclasses.dataclass
+class SysInfoBase:
+    uptime: Uptime
+    loads: typing.Tuple[int, int, int]
+    totalram: int
+    freeram: int
+    sharedram: int
+    bufferram: int
+    totalswap: int
+    freeswap: int
+    procs: int
+
+
+class SysInfo(SysInfoBase):
+    def __init__(self):
+        fmt = 'l3L6LH22s'
+        buf = ctypes.create_string_buffer(struct.calcsize(fmt) + 1)
+        libc.sysinfo(buf)
+        uptime_seconds, loads_1, loads_5, loads_15, totalram, freeram, \
+            sharedram, bufferram, totalswap, freeswap, procs, \
+            *padding = struct.unpack_from(fmt, buf.raw)
+        uptime = Uptime.from_total_seconds(uptime_seconds)
+        super().__init__(
+            uptime, (loads_1, loads_5, loads_15), totalram, freeram,
+            sharedram, bufferram, totalswap, freeswap, procs,
+        )
+
+
+@app.route('/api/sysinfo')
+def sysinfo():
+    return flask.jsonify(SysInfo())
