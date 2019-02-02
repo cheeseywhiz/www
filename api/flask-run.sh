e1625fe52bc5865d5cc9f670e28cc52e2174b65c
Spencer Todd <cheesy.mac.dott@gmail.com>
2019-02-01 23:24:24 -0500

    Add check for existing tmux sessions
    Otherwise the environment won't be set at all

--- api/flask-run.sh
+++ api/flask-run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # Start development server in its own environment
 
+die() {
+	echo $@
+	exit 1
+}
+
 flask-run() {
 	source ~/www/api/venv/bin/activate
 	export FLASK_APP=$HOME/www/api/app.py
@@ -8,5 +13,6 @@ flask-run() {
 	tmux new -d -s app flask run
 }
 
+tmux ls >& /dev/null && die Close other tmux sessions first
 (flask-run)
 exec tmux a -t app
